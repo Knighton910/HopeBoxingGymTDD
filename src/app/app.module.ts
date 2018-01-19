028ce5aca179d338400fe6e6ad8df26be38a925d
🦁 Kelvin-Knighton <keltheceo@gmail.com>
2018-01-19 14:05:09 -0800

    added paragrahs & signINout functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
